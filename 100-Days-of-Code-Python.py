a03885ad7ab60bf5b9a09631fd5c45e6db74c6a0
jameskalenga-cloud <james.kalenga@cloudsmiths.ai>
2025-04-04 20:04:09 +0200

    Guessing game

--- 100-Days-of-Code-Python.py
+++ 100-Days-of-Code-Python.py
@@ -209,54 +209,88 @@
 #         game_control = False
 
 
-student_scores = {
-    'Harry': 88,
-    'Ron': 78,
-    'Hermione': 95,
-    'Draco': 75,
-    'Neville': 60
-}
+# student_scores = {
+#     'Harry': 88,
+#     'Ron': 78,
+#     'Hermione': 95,
+#     'Draco': 75,
+#     'Neville': 60
+# }
 
-student_grades = {}
+# student_grades = {}
 
-for item in student_scores:
-    print(item)
+# for item in student_scores:
+#     print(item)
     
-    if student_scores[item] >= 91 and student_scores[item] <=100:
-        student_grades[item] = "Outstanding"
-    elif student_scores[item] >=81 and student_scores[item] <=90:
-        student_grades[item] = "Exceeds Expectations"
-    elif student_scores[item] >=71 and student_scores[item] <=80:
-        student_grades[item] = "Acceptable"
-    elif student_scores[item] <= 70:
-        student_grades[item] = "Fail"
+#     if student_scores[item] >= 91 and student_scores[item] <=100:
+#         student_grades[item] = "Outstanding"
+#     elif student_scores[item] >=81 and student_scores[item] <=90:
+#         student_grades[item] = "Exceeds Expectations"
+#     elif student_scores[item] >=71 and student_scores[item] <=80:
+#         student_grades[item] = "Acceptable"
+#     elif student_scores[item] <= 70:
+#         student_grades[item] = "Fail"
         
-print(student_grades)
+# print(student_grades)
 
 
 
-travel_log = {
-    "France": ["Paris", "Lille", "Dijon"],
-    "Germany":["Stuttgart", "Berlin"]
+# travel_log = {
+#     "France": ["Paris", "Lille", "Dijon"],
+#     "Germany":["Stuttgart", "Berlin"]
 
+# }
+
+# print(travel_log["France"][1])
+
+# nested_list = ["A", "B", ["C", "D"]]
+
+# print(nested_list[2][1])
+
+
+# travel_log = {
+#     "France":{
+#         "cities_visited": ["Paris", "Lille","Dijon"],
+#         "total_visits": 12
+#     },
+#     "Germany":{
+#         "cities_visited": ["Berlin", "Humburg","Stuttgart"],
+#         "total_visits": 5
+#     }
+# }
+
+# print(travel_log["Germany"]["cities_visited"][2])
+
+
+import random
+print("Welcome to the Number Guessing Game!")
+print("I am thinking of a number between 1 and 100")
+game_mode = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+game_attepts ={
+    "easy": 10,
+    "hard": 5,
+    "modes": ["Too high", "Too low", "You win", "You lose"]
 }
 
-print(travel_log["France"][1])
+game_control = True
+magic_number = random.randint(1, 100)
+while game_control:
 
-nested_list = ["A", "B", ["C", "D"]]
+    print(f"You have {game_attepts[game_mode]} attempts remaining to guess the number.")
+    player_guess = int(input("Make a guess: "))
 
-print(nested_list[2][1])
+    if player_guess == magic_number:
+        print(game_attepts["modes"][2])
+        game_control = False
+        continue
+    elif player_guess > magic_number:
+        print(game_attepts["modes"][0])
+    else:
+        print(game_attepts["modes"][1])
 
-
-travel_log = {
-    "France":{
-        "cities_visited": ["Paris", "Lille","Dijon"],
-        "total_visits": 12
-    },
-    "Germany":{
-        "cities_visited": ["Berlin", "Humburg","Stuttgart"],
-        "total_visits": 5
-    }
-}
-
-print(travel_log["Germany"]["cities_visited"][2])
+    if game_attepts[game_mode] == 1:
+        game_control = False
+        print(game_attepts["modes"][3])
+    
+    game_attepts[game_mode] -=1 
